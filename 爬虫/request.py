a2a0082f9e364ba06e00531569dae9d6a749b3b5
wangx <wangxuan1@shanghaitech.edu.cn>
2019-09-20 22:34:55 +0800

    requst

--- 爬虫/request.py
+++ 爬虫/request.py
@@ -29,4 +29,38 @@ print(r.text)#可以看到url中的链接添加了信息
 
 #解析结果得到字典格式
 print(r.json())
-print(type(r.json()))
+print(type(r.json()))#需要注意的是如果返回的不是JSON格式的话就会报错
+
+###抓取网页
+import requests
+import re
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko)Chrmoe/52.0.2743 Safari/537.36'
+}
+r = requests.get('https://www.zhihu.com/explore', headers=header)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)
+print(r.text)
+titles = re.findall(pattern, r.text)
+print(titles)
+
+###抓取二进制文件
+import requests
+r = requests.get("https://github.com/favicon.ico")
+print(r.text)
+print(r.content)
+with open('favicon.ico', 'wb') as f:
+    f.write(r.content)
+    
+#添加headers
+import requests
+header = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko)Chrmoe/52.0.2743 Safari/537.36'
+}
+r = requests.get('http://www.zhihu.com/explore', headers = header)
+print(r.text)
+
+#POST请求
+import requests
+data = {'name':'germey', 'age':'22'}
+r = requests.post
