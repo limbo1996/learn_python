3cb87c87a0ea06a49b24e4ed620b696a07f4d098
wangx <44095085+limbo1996@users.noreply.github.com>
2019-09-28 19:55:49 +0800

    maoyan

--- 爬虫/request.py
+++ 爬虫/request.py
@@ -113,6 +113,66 @@ r = requests.get('http://localhost:5000', auth = HTTPBasicAuth('username', 'pass
 
 
 
+#测试
+import json
+from requests.exceptions import RequestException
+import re
+import time
+import requests
+
+def get_one_page(url): #获取页面信息
+    try:
+        headers = {
+            'User-Agent':'Mozilla/5.0(Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36(KHTML, like Gecko)Chrmoe/65.0.3325.162 Safari/537.36'
+        }
+        response = requests.get(url, headers = headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+    
+def parse_one_page(html):#分析第一页
+    pattern = re.compile('<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name".*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    print(items)
+    for item in items:
+
+        yield{
+            'index' : item[0],
+            'image' : item[1],
+            'title' : item[2],
+            'actor' : item[3].strip()[3:],
+            'time' : item[4].strip()[5:],
+            'score' : item[5] + item[6]
+        }
+    
+def write_to_file(content):#写入文件
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dump(content, ensure_ascii=False) + '\n')
+
+def main(offset):#读取其余页面
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+if __name__ == '__main__':
+    for i in range(10):
+        main(offset = i * 10)
+        time.sleep(1)
 
 
+
+
+
+
+
+def main():
+    url = 'http://maoyan.com/board/4'
+    html = get_one_page(url)
+    print(html)
+
+main()
 
