9f405c03d2fcb8ccfdbf719293f7e1993bd83022
limbo1996 <wangxuan1@shanghaitech.edu.cn>
2020-07-03 10:12:20 +0800

    array

---
+++ python编程/数组.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  2 20:17:23 2020
+
+@author: limbo
+"""
+
+# 数组
+from array import array
+# 数组
+primes = array("i", [2,3,4,5,6,7,8]) #需要一个类型代码， “i”表示是一个整型数组
+
+import numpy as np
+
+test = np.array([2, 3, 4])
+test2 = np.array([[2, 3], [4]], dtype=object)
