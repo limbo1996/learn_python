1e38f06e439e733eeba40fc38694ad7bee784446
limbo1996 <wangxuan1@shanghaitech.edu.cn>
2021-03-16 21:35:58 +0800

    栈及基本应用括号匹配

--- 数据结构/线性结构.py
+++ 数据结构/线性结构.py
@@ -1,23 +1,10 @@
-import requests
-
-r = requests.get('https://search.jd.com/Search?keyword=ipad&enc=utf-8&wq=&pvid=1e8a937fe0c54ad686f61459ce1b6c28')
-
-
-r.status_code
-
-r.encoding
-
-r.text
-
-
-
 import time
 
 time.time()
-# 1970年1月1日开始，单位是秒
+# 
 
 
-# 累计求和程序时间检测
+# 
 def sumOfN(n):
   start = time.time()
   theSum = 0
@@ -33,9 +20,7 @@ for i in range(5):
         % sumOfN(1000000))
         
         
-        
-        
-# 无迭代算法
+# 
 def sumOfN2(n):
   start = time.time()
   theSum = (n * (n + 1)) / 2
@@ -45,4 +30,18 @@ def sumOfN2(n):
 for i in range(5):
   print("Sum is %d required %f seconds" 
         % sumOfN2(100000))
-    
+   
+   
+   
+# Stack
+from Stack import Stack
+s = Stack()
+
+print(s.isEmpty())
+s.push(4)
+s.push("dog")
+print(s.peek())
+s.pop()
+s.peek()
+
+
