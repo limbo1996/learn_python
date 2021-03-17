302f93a703a8a0bc48ec54aabc783ad9e34a699b
limbo1996 <wangxuan1@shanghaitech.edu.cn>
2021-03-17 23:35:01 +0800

    queue

---
+++ 数据结构/baseConverter.py
@@ -0,0 +1,23 @@
+from Stack import Stack
+
+def baseConverter(decNumber, base):
+  digits = "0123456789ABCDEF"
+  
+  remstack = Stack()
+  
+  while decNumber > 0:
+    rem = decNumber % base # 求余数
+    remstack.push(rem)
+    decNumber = decNumber // base
+    
+  newString = ""
+  
+  while not remstack.isEmpty():
+    newString = newString + digits[remstack.pop()]
+    
+  return newString
+    
+
+print(baseConverter(25, 2))
+
+print(baseConverter(30, 16))
